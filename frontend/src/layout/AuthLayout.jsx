b69c69b34ef829f662c8bf289bf6b7d51fe901b9
martaalcazarr <martaalcazarredondo@gmail.com>
2023-02-19 23:37:25 -0300

    actualización authlayout e index.html

--- frontend/src/layout/AuthLayout.jsx
+++ frontend/src/layout/AuthLayout.jsx
@@ -4,10 +4,11 @@ import {Outlet} from 'react-router-dom'
 const AuthLayout = () =>{
     return (
         <>
-        <h1>Administrador de pacientes de Veterinario</h1>
-        {/*outlet es un espacio para los hijos del AuthLayout*/}
-        <Outlet/>
         
+        <main className="container mx-auto md:grid grid-cols-2 mt-12 gap-10 p-5 items-center">
+        {/*outlet es un espacio reservado para los componentes de los hijos del AuthLayout*/}
+        <Outlet/>
+        </main>
         </>
     );
 }
