04b453c0af2473ceb8403c9ed26ff83b4ca8b1a9
martaalcazarr <martaalcazarredondo@gmail.com>
2023-02-20 20:51:29 -0300

    pagina pwolvidada y registro, alertas

--- frontend/src/paginas/PasswordOlvidada.jsx
+++ frontend/src/paginas/PasswordOlvidada.jsx
@@ -1,9 +1,36 @@
+import {Link} from 'react-router-dom';
 const PasswordOlvidada = () =>{
     return(
         <>
         
-            <div>1</div>
-            <div>2</div>
+        <div>
+            <h1 className="text-indigo-600 font-black text-6xl">Recupera tu acceso y no pierdas tus {""} <span className="text-black">pacientes</span></h1>
+        </div>
+        <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
+            <form>
+            <div className="my-5">
+                    <label className="uppercase text-gray-600 block text-xl font-bold">
+                        Email
+                    </label>
+                    <input 
+                    type="email"
+                    placeholder="Tu email"
+                    className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    />
+                </div>
+                <input 
+                type="submit"
+                value="Enviar instrucciones"
+                className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+                />
+            </form>
+            <nav className='mt-10 lg:flex lg:justify-between'>
+                <Link className='block text-center my-5 text-gray-500'
+                    to="/">¿Ya tienes una cuenta? Inicia sesión</Link>
+                <Link className='block text-center my-5 text-gray-500'
+                    to="/registrar">¿No tienes una cuenta? Regístrate</Link>
+            </nav>
+        </div>
            
             </>
         )
--- frontend/src/paginas/Registrar.jsx
+++ frontend/src/paginas/Registrar.jsx
@@ -1,12 +1,42 @@
+import { useState } from 'react'
 import {Link} from 'react-router-dom';
+import Alerta from '../components/Alerta';
+
 const Registrar = () => {
+    const [nombre, setNombre] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [repetirPassword, setRepetirPassword] = useState('');
+
+    const [alerta, setAlerta] = useState({});
+    
+    const handleSubmit = e => {
+        e.preventDefault()
+        if([nombre, email, password, repetirPassword].includes('')){
+            setAlerta({msg: 'Hay campos vacíos', error:true})
+        return
+        };
+        if(password !== repetirPassword){
+            setAlerta({msg: 'Las contraseñas no coinciden', error:true})
+            return;
+        }
+        if(password.length < 6){
+            setAlerta({msg: 'La contraseña es muy corta, debe tener mínimo 6 carácteres', error:true})
+            return;
+        }
+    }
+   
+
     return (
         <>
         <div>
             <h1 className="text-indigo-600 font-black text-6xl">Crea tu cuenta y administra tus {""} <span className="text-black">pacientes</span></h1>
         </div>
         <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-            <form>
+            <Alerta 
+            alerta={alerta}/>
+            <form 
+            onSubmit={handleSubmit}>
             <div className="my-5">
                     <label className="uppercase text-gray-600 block text-xl font-bold">
                         Nombre
@@ -15,6 +45,8 @@ const Registrar = () => {
                     type="text"
                     placeholder="Tu nombre"
                     className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    value={nombre}
+                    onChange={e => setNombre(e.target.value)}
                     />
                 </div>
                 <div className="my-5">
@@ -25,6 +57,8 @@ const Registrar = () => {
                     type="email"
                     placeholder="Email de registro"
                     className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    value={email}
+                    onChange={e => setEmail(e.target.value)}
                     />
                 </div>
                 <div className="my-5">
@@ -35,6 +69,8 @@ const Registrar = () => {
                     type="password"
                     placeholder="Tu contraseña"
                     className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    value={password}
+                    onChange={e => setPassword(e.target.value)}
                     />
                 </div>
                 <div className="my-5">
@@ -45,11 +81,13 @@ const Registrar = () => {
                     type="password"
                     placeholder="Repite tu contraseña"
                     className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    value={repetirPassword}
+                    onChange={e => setRepetirPassword(e.target.value)}
                     />
                 </div>
                 <input 
                 type="submit"
-                value="Iniciar sesión"
+                value="Crear cuenta"
                 className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
                 />
             </form>
