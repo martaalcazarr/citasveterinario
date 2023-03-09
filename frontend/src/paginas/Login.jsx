199fea3c026eb8e06f1519292a8d4b980fef7a61
martaalcazarr <martaalcazarredondo@gmail.com>
2023-03-09 00:49:49 -0300

    autenticacion jwt

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -1,8 +1,41 @@
-import {Link} from 'react-router-dom';
+import {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom';
+import Alerta from '../components/Alerta'
 import useAuth from '../hooks/useAuth'
+import clienteAxios from '../config/axios'
 
 const Login = () => {
 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [alerta, setAlerta] = useState({})
+
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        if([email, password].includes('')){
+            setAlerta({
+                msg: 'Todos los campos son obligatorios',
+                error: true
+            })
+            return
+        }
+        try {
+            const {data} = await clienteAxios.post('/veterinarios/login', {email, password})
+            localStorage.setItem('token', data.token)
+
+            navigate('/admin')
+        } catch (error) {
+            setAlerta({
+                msg: error.response.data.msg,
+                error: true
+            })
+        }
+    }
+
+    const {msg} = alerta
     
     return (
         <>
@@ -11,40 +44,50 @@ const Login = () => {
             <h1 className="text-indigo-600 font-black text-6xl">Inicia sesión y administra tus {""} <span className="text-black">pacientes</span></h1>
         </div>
         <div className='mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white'>
-            <form>
-                <div className="my-5">
-                    <label className="uppercase text-gray-600 block text-xl font-bold">
-                        Email
-                    </label>
+            {msg && <Alerta 
+                alerta={alerta} 
+            />}    
+                    
+                <form onSubmit={handleSubmit}>
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Email
+                        </label>
+                        <input 
+                        type="email"
+                        placeholder="Email de registro"
+                        className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        
+                        />
+                    </div>
+                    <div className="my-5">
+                        <label className="uppercase text-gray-600 block text-xl font-bold">
+                            Contraseña
+                        </label>
+                        <input 
+                        type="password"
+                        placeholder="Tu contraseña"
+                        className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+
+                        />
+                    </div>
                     <input 
-                    type="email"
-                    placeholder="Email de registro"
-                    className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+                    type="submit"
+                    value="Iniciar sesión"
+                    className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
                     />
-                </div>
-                <div className="my-5">
-                    <label className="uppercase text-gray-600 block text-xl font-bold">
-                        Contraseña
-                    </label>
-                    <input 
-                    type="password"
-                    placeholder="Tu contraseña"
-                    className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
-                    />
-                </div>
-                <input 
-                type="submit"
-                value="Iniciar sesión"
-                className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
-                />
-    
-            </form>
-            <nav className='mt-10 lg:flex lg:justify-between'>
-                <Link className='block text-center my-5 text-gray-500'
-                    to="/registrar">¿No tienes una cuenta? Regístrate</Link>
-                <Link className='block text-center my-5 text-gray-500'
-                    to="/password-olvidada">He olvidado mi contraseña</Link>
-            </nav>
+        
+                </form>
+                <nav className='mt-10 lg:flex lg:justify-between'>
+                    <Link className='block text-center my-5 text-gray-500'
+                        to="/registrar">¿No tienes una cuenta? Regístrate</Link>
+                    <Link className='block text-center my-5 text-gray-500'
+                        to="/password-olvidada">He olvidado mi contraseña</Link>
+                </nav>
         </div>
         
         </>
