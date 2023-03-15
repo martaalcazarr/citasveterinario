91bc71ade921aa0dcc40973c1e09d133e02ae499
martaalcazarr <martaalcazarredondo@gmail.com>
2023-03-14 21:43:51 -0300

    formulario editar perfil

--- frontend/src/paginas/EditarPerfil.jsx
+++ frontend/src/paginas/EditarPerfil.jsx
@@ -17,8 +17,40 @@ const EditarPerfil = () => {
               <div className="my-3">
                 <label className="uppercase font-bold text-gray-600">Nombre</label>
                   <input
+                  type="text"
+                  className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                  name="nombre"
                   />
               </div>
+              <div className="my-3">
+                <label className="uppercase font-bold text-gray-600">Sitio web</label>
+                  <input
+                  type="text"
+                  className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                  name="web"
+                  />
+              </div>
+              <div className="my-3">
+                <label className="uppercase font-bold text-gray-600">Teléfono</label>
+                  <input
+                  type="text"
+                  className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                  name="telefono"
+                  />
+              </div>
+              <div className="my-3">
+                <label className="uppercase font-bold text-gray-600">Email</label>
+                  <input
+                  type="email"
+                  className="border bg-gray-50 w-full p-2 mt-5 rounded-lg"
+                  name="email"
+                  />
+              </div>
+              <input 
+                type="submit"
+                value="Guardar cambios"
+                className="bg-indigo-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5"
+              />
             </form>
           </div>
         </div>
